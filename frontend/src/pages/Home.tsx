15d2a49f13241c9c92abdc3f95d04396ab1cd610
Duke Norsworthy <duke.norsworthy@gmail.com>
2026-03-10 23:31:11 -0400

    initial

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,19 @@
+export function Home() {
+
+    const user = true
+
+    return (
+        <div className="home">
+            {user ? (
+                <div className="welcome">
+                    <h1>Welcome!</h1>
+                </div>
+            ) : (
+                <div className="welcome">
+                    <h1>Log In</h1>
+                    <p>Please log in to manage your appeals.</p>
+                </div>
+            )}
+        </div>
+    )
+}
